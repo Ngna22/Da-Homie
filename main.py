1dc79bbc6d2645fcbc70c119c87678e99ee16fe0
Ngna22 <yyehe7909@gmail.com>
2023-09-05 14:01:04 +0300

    ng

--- main.py
+++ main.py
@@ -95,34 +95,26 @@ async def say_error(ctx, error):
 
 
 
-sent_image_urls = set()
-
-@bot.command(name="ser", aliases=["search"])
-async def search_image(ctx, *, search_query: str):
-    search_query_encoded = urllib.parse.quote(search_query)
-    search_url = f"https://www.google.com/search?q={search_query_encoded}&tbm=isch&safe=active"
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
-    response = requests.get(search_url, headers=headers)
-    soup = BeautifulSoup(response.text, "html.parser")
-    img_tags = soup.find_all("img")
-
-    image_urls = [img["src"] for img in img_tags if "src" in img.attrs and img["src"] not in sent_image_urls and not img["src"].endswith(".gif")]
-
-    if image_urls:
-        selected_image_url = random.choice(image_urls)
-        sent_image_urls.add(selected_image_url)
-
-        search_query_decoded = urllib.parse.unquote(search_query)
-        embed = discord.Embed(color=0x9FC6F6, title=f"You searched for: {search_query_decoded}")
-        embed.set_image(url=selected_image_url)
-
-        await ctx.send(embed=embed)
-    else:
-        await ctx.send("wtf were you searching☠️.")
 
 
 
 
+
+
+
+
+
+
+
+@bot.command()
+async def echo_dm(ctx, target_user: discord.User, *, message_to_send=None):
+    # Delete the user's command message
+    await ctx.message.delete()
+
+    try:
+        await target_user.send(message_to_send)
+    except discord.Forbidden:
+        await ctx.send("I cannot send messages to a user's DMs.")
 
 
 
